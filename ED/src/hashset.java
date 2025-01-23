5869f358b6e947b61b5cfc1074c0002385960fa6
germanpart <german.ps@outlook.com>
2025-01-23 08:36:04 -0700

    German

---
+++ ED/src/hashset.java
@@ -0,0 +1,44 @@
+
+import java.util.HashSet;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author germa
+ */
+public class hashset {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+     HashSet<String> lista=new HashSet<>();
+     
+     lista.add("1");
+     lista.add("2");
+     lista.add("3");
+     lista.add("4");
+     lista.add("5");
+     lista.add("6");
+     lista.add("7");
+     lista.add("8");
+     lista.add("9");
+     lista.add("10");
+     lista.add("11");
+     lista.add("12");
+     lista.add("13");
+     lista.add("14");
+     lista.add("15");
+     lista.add("16");
+     
+        
+            System.out.println(lista);
+        }
+    }
+    
+
